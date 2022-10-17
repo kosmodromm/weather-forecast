a4911728fdcc0f7a9625315da394b107dfec40d0
Anton Kizerev <a.kizerev@gmail.com>
2022-10-17 15:08:23 +0300

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: [
+    ({ addComponents }) => {
+      const AppLogo = {
+        '.App-logo': {
+          animation: 'App-logo-spin infinite 20s linear',
+          height: '40vmin',
+          'pointer-events': 'none'
+        },
+        '@keyframes App-logo-spin': {
+          from: {
+            transform: 'rotate(0deg)'
+          },
+          to: {
+            transform: 'rotate(360deg)'
+          }
+        }
+      }
+      addComponents(AppLogo)
+    }
+  ]
+}
