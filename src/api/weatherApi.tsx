c12e4d8cc7a573c97bbc52011c9bfa6dd685500c
Anton Kizerev <a.kizerev@gmail.com>
2022-10-21 17:25:37 +0300

    change weather api

--- src/api/weatherApi.tsx
+++ src/api/weatherApi.tsx
@@ -3,67 +3,61 @@ export interface ICoords {
   longitude: number
 }
 
-export interface IWeatherDTO {
-  'coord': {
-    'lon': 10.99,
-    'lat': 44.34
-  },
-  'weather': [
-    {
-      'id': 501,
-      'main': 'Rain',
-      'description': 'moderate rain',
-      'icon': '10d'
-    }
-  ],
-  'base': 'stations',
-  'main': {
-    'temp': 298.48,
-    'feels_like': 298.74,
-    'temp_min': 297.56,
-    'temp_max': 300.05,
-    'pressure': 1015,
-    'humidity': 64,
-    'sea_level': 1015,
-    'grnd_level': 933
-  },
-  'visibility': 10000,
-  'wind': {
-    'speed': 0.62,
-    'deg': 349,
-    'gust': 1.18
-  },
-  'rain': {
-    '1h': 3.16
-  },
-  'clouds': {
-    'all': 100
-  },
-  'dt': 1661870592,
-  'sys': {
-    'type': 2,
-    'id': 2075663,
-    'country': 'IT',
-    'sunrise': 1661834187,
-    'sunset': 1661882248
-  },
-  'timezone': 7200,
-  'id': 3163858,
-  'name': 'Zocca',
-  'cod': 200
+export interface IForecastHour {
+  time: string,
+  temp_c: number,
+  condition: {
+    icon: string
+  }
 }
 
-const API_KEY = 'f953a91cfb30c44995dd52bb97a21548';
+export interface IForecastDay {
+  date: string,
+  day: {
+    maxtemp_c: number,
+    mintemp_c: number,
+    avgtemp_c: number,
+    totalsnow_cm: number,
+    condition: {
+      icon: string
+    },
+    astro: {},
+    hour: IForecastHour[]
+  }
+}
 
-/*'https://api.openweathermap.org/data/3.0/onecall?'*/
-/*'https://api.openweathermap.org/data/2.5/weather?'*/
-const API_URL = new URL('https://api.openweathermap.org/data/3.0/onecall?');
+export interface IWeatherDTO {
+  location: {
+    name: string,
+    region: string,
+    country: string,
+    lat: number,
+    lon: number,
+    tz_id: string,
+    localtime_epoch: number,
+    localtime: string
+  },
+  current: {
+    temp_c: number,
+    condition: {
+      icon: string
+    },
+    uv: number
+  },
+  forecast: {
+    forecastday: IForecastDay[]
+  }
+}
+
+const API_KEY = '89f5cbfe7d4748bfbdc101556222010';
+const DAYS = 10;
+const API_URL = new URL('http://api.weatherapi.com/v1/forecast.json?');
 
 export async function getWeatherByCoords(coords: ICoords): Promise<IWeatherDTO> {
   let weatherData: IWeatherDTO;
 
   try {
-    const data = await fetch(API_URL + `lat=${coords.latitude}&lon=${coords.longitude}&exclude=minutely&appid=${API_KEY}`);
+    const data = await fetch(API_URL + `key=${API_KEY}&q=${coords.latitude},${coords.longitude}&days=${DAYS}&aqi=no&alerts=no`);
     weatherData = await data.json();
     return weatherData;
   } catch (error) {
@@ -75,14 +69,10 @@ export async function getWeatherByCity(city: string): Promise<IWeatherDTO> {
   let weatherData: IWeatherDTO;
 
   try {
-    const data = await fetch(API_URL + `q=${city.toLowerCase()}&appid=${API_KEY}`);
+    const data = await fetch(API_URL + `key=${API_KEY}&q=${city.toLowerCase()}&days=${DAYS}&aqi=no&alerts=no`);
     weatherData = await data.json();
     return weatherData;
   } catch (error) {
     throw error;
   }
-}
-
-export function getWeatherIcon(code: string): string {
-  return `http://openweathermap.org/img/wn/${code}@2x.png`;
 }
