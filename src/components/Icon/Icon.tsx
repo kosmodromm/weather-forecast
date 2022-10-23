f4f52bdd896faa47bc2bafaa0d03a6f0f4db6690
Anton Kizerev <a.kizerev@gmail.com>
2022-10-23 23:29:42 +0300

    cleanup

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -16,18 +16,6 @@ interface IProps {
 
 const Icon: React.FC<IProps> = ({ name, className }) => {
   const SVGIcon = icons[name];
-  /*let svgWidth = 30;
-  let svgHeight = 30;
-  let svgFill = 'white';
-  let svgStroke = 'none';
-
-  if (className) {
-    const svgStyles = className?.split(' ');
-    svgWidth = parseFloat(svgStyles[0]?.slice(6)) || 30;
-    svgHeight = parseFloat(svgStyles[1]?.slice(7)) || 30;
-    svgFill = svgStyles[2]?.slice(5) || 'white';
-    svgStroke = svgStyles[3]?.slice(7) || 'none';
-  }*/
 
   return (
     <i className={
